45ac883c974d5ac8a259e82dfde84d896de226ab
thisRafael <r.alvarado1693@gmail.com>
2023-08-17 19:09:35 -0500

    peluqueria canina

---
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,14 @@
+package com.mycompany.peluqueriacanina;
+
+import com.mycompany.peluqueriacanina.igu.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+
+    }
+}
